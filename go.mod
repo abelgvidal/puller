4911e604cf3daee5e881fd9e5cf82c8dff31380e
Abel Gonzalez <abel@nextail.co>
2023-03-15 00:56:30 +0100

    Now sending messages to slack!

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module puller
 
 go 1.19
 
-require gopkg.in/yaml.v2 v2.4.0 // indirect
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/slack-go/slack v0.12.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- puller.go
+++ puller.go
@@ -6,12 +6,15 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/slack-go/slack"
 	"gopkg.in/yaml.v2"
 )
 
 type Config struct {
-	Repos []string `yaml:"repos"`
-	Token string
+	Repos        []string `yaml:"repos"`
+	Slacktoken   string
+	Githubtoken  string
+	Slackchannel string
 }
 
 type PullRequest struct {
@@ -19,13 +22,33 @@ type PullRequest struct {
 	URL   string `json:"html_url"`
 }
 
-func getPullRequestsFromOneRepo(repo string, token string) ([]PullRequest, error) {
+func sendToSlack(texto string, config Config) error {
+	api := slack.New(config.Slacktoken)
+	attachment := slack.Attachment{
+		Text: texto,
+	}
+
+	_, timestamp, err := api.PostMessage(
+		config.Slackchannel,
+		slack.MsgOptionAttachments(attachment),
+		slack.MsgOptionAsUser(true),
+	)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return err
+	}
+	fmt.Printf("Message successfully sent to channel %s at %s", config.Slackchannel, timestamp)
+
+	return nil
+}
+
+func getPullRequestsFromOneRepo(repo string, Githubtoken string) ([]PullRequest, error) {
 	req, err := http.NewRequest("GET", fmt.Sprintf("https://api.github.com/repos/%s/pulls", repo), nil)
 	if err != nil {
 		panic(err)
 	}
 
-	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", Githubtoken))
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
@@ -47,7 +70,7 @@ func getPullRequests(config Config) ([]PullRequest, error) {
 	var totalPullRequests []PullRequest
 
 	for _, repo := range config.Repos {
-		pullRequests, _ := getPullRequestsFromOneRepo(repo, config.Token)
+		pullRequests, _ := getPullRequestsFromOneRepo(repo, config.Githubtoken)
 		totalPullRequests = append(totalPullRequests, pullRequests...)
 	}
 	return totalPullRequests, nil
@@ -73,7 +96,14 @@ func main() {
 	config := getConfig()
 	pullRequests, _ := getPullRequests(config)
 
+	if len(pullRequests) > 0 {
+		sendToSlack("¡Qué momento más maravilloso para revisar los pull requests pendientes!", config)
+	}
 	for _, pr := range pullRequests {
+		err := sendToSlack(fmt.Sprintf("Title: %s\nURL: %s\n\n", pr.Title, pr.URL), config)
+		if err != nil {
+			fmt.Print("Error sending to slack:", err)
+		}
 		fmt.Printf("Title: %s\nURL: %s\n\n", pr.Title, pr.URL)
 	}
 }
